81f3df613f814befe35144471efd6d751c8415ec
Lucas Tepper <lucas@kryten.nl>
2023-03-28 20:54:14 +0200

    save and added requirements.txt

--- dag2/huiswerk/spelenmetwoorden.py
+++ dag2/huiswerk/spelenmetwoorden.py
@@ -15,37 +15,49 @@ lijst_met_palindromen = []
 alle_woorden = set()
 alle_omgekeerde_woorden = set()
 
-for woord in lijst_met_woorden:
-  # debug print woord
-  #print(woord)
-  # tellen langste woord
-  lengte = len(woord)
-  if lengte > langstelengte:
-    langstelengte = lengte
-    langstewoord = woord
+def verzamel_wat_statistieken():
+  global langestewoord, langstelengte, aantal_woorden, alle_woorden
+  for woord in lijst_met_woorden:
+    # debug print woord
+    #print(woord)
+    # tellen langste woord
+    lengte = len(woord)
+    if lengte > langstelengte:
+      langstelengte = lengte
+      langstewoord = woord
   
-  # bepalen palindroom
-  woord_omgekeerd = ""
-  woord_omgekeerd_lijst = reversed(woord)
-  for letter in woord_omgekeerd_lijst:
-    woord_omgekeerd += letter
+    # bepalen palindroom
+    woord_omgekeerd = ""
+    woord_omgekeerd_lijst = reversed(woord)
+    for letter in woord_omgekeerd_lijst:
+      woord_omgekeerd += letter
 
-  # maken sets
-  alle_woorden.add(woord)
-  alle_omgekeerde_woorden.add(woord_omgekeerd)
+    # maken sets
+    alle_woorden.add(woord)
+    alle_omgekeerde_woorden.add(woord_omgekeerd)
 
-  if woord_omgekeerd == woord:
-    #print(f"Palindroom gevonden: {woord}")
-    lijst_met_palindromen.append(woord) 
+    if woord_omgekeerd == woord:
+      #print(f"Palindroom gevonden: {woord}")
+      lijst_met_palindromen.append(woord) 
 
-  # tellen woorden, maar je kan ook lengte lijst bepalen om zelfde resultaat te krijgen
-  aantal_woorden = aantal_woorden + 1
+    # tellen woorden, maar je kan ook lengte lijst bepalen om zelfde resultaat te krijgen
+    aantal_woorden = aantal_woorden + 1
+
+  print(f"Aantal woorden in bestand {bestand} is {aantal_woorden}")
+  # langste woord
+  print(f"Langste woord is {langstewoord} met lengte {str(langstelengte)}")
+  # palindromen gevonden
+  print(f"Aantal gevonden palindroom woorden is {len(lijst_met_palindromen)}")
+  # vergelijk de 2 sets wat ze gemeenschappelijk hebben
+  print(f"Alle woorden die omgekeerd ook bestaan zijn er {len(alle_omgekeerde_woorden.intersection(alle_woorden))}")
+
+def vraag_om_woord():
+  mijnwoord = input("Vul een woord in: ")
+  if (mijnwoord in alle_woorden):
+    print(f"Woord {mijnwoord} is aanwezig in de lijst")
+  else:
+    print(f"Kan woord {mijnwoord} niet vinden in de lijst!")
 
 
-print(f"Aantal woorden in bestand {bestand} is {aantal_woorden}")
-# langste woord
-print(f"Langste woord is {langstewoord} met lengte {str(langstelengte)}")
-# palindromen gevonden
-print(f"Aantal gevonden palindroom woorden is {len(lijst_met_palindromen)}")
-# vergelijk de 2 sets wat ze gemeenschappelijk hebben
-print(f"Alle woorden die omgekeerd ook bestaan zijn er {len(alle_omgekeerde_woorden.intersection(alle_woorden))}")
+verzamel_wat_statistieken()
+vraag_om_woord()
