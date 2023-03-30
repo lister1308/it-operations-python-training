c83c8e3ea4dddcf70301b13e0208dc7d42cb6f4b
Bert Sijtsma <b.sijtsma@home.nl>
2023-03-30 10:45:21 +0200

    Wijzigingen van bert

--- dag3/kat/waarisdekat.py
+++ dag3/kat/waarisdekat.py
@@ -21,10 +21,31 @@ def controleer_inhoud(doos,dooskat):
     # kijk of opgegeven doos is doos waar kat zit
     # geef terug True of False 
 
-def verplaats_kat(dooskat):
+def verplaats_kat(doos_met_kat):
     # verplaats de kat, 1 naar links of rechts
     # als in 1, kan je alleen naar 2, als in aantal_dozen, dan alleen naar links
     # geeft nieuwe doos terug
+
+    '''als doos_met_kat is 1 dan
+          doos_met_kat ← doos_met_kat + 1
+       anders als doos_met_kat is aantal_dozen dan
+         doos_met_kat ← doos_met_kat – 1
+       anders
+          doos_met_kat ← doos_met_kat + toevalsgetal: -1 of 1
+     '''
+    
+    if doos_met_kat == 1:
+        # verplaats kat naar rechts
+        doos_met_kat += 1
+    elif doos_met_kat == aantal_dozen:
+        # verplaats kat naar links
+        doos_met_kat -= 1
+    else:
+        # -1 is naar links, 1 is naar rechts
+        keuze=[-1,1]
+        doos_met_kat += random.choice(keuze)
+
+
 
 # lus die loopt zolang kat gevonden = False
 # doos_met_kat = verstop_kat()
