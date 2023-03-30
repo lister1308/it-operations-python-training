e2e781c9a035fa25f1b250bd21b71b406f0b723b
Lucas Tepper <lucas@kryten.nl>
2023-03-30 11:11:55 +0200

    versie x

--- dag3/kat/waarisdekat.py
+++ dag3/kat/waarisdekat.py
@@ -11,11 +11,25 @@ kat_gevonden = False
 
 def verstop_kat():
     # maak functie die een random getal terug geeft tussen 1 en max aantal dozen
+    return
 
 def welke_doos():
     # vraag gebruiker om een doos op te geven tussen 1 en max aantal_dozen
     # doos 0 = stoppen
     # geef terug doos nummer
+    doos = 1 # zet op 1 om de lus in te gaan
+    while doos != 0 or doos > aantal_dozen:
+        doos = input(f"In welke doos zit de kat (tik 0 om te stoppen, doos tussen 1 en {aantal_dozen})? ")
+        if not doos.isnumeric():
+            print("Je moet een cijfer opgeven!")
+            doos = 1
+            continue
+        elif int(doos) > aantal_dozen or int(doos) < 0: 
+            print("Cijfer moet tussen 1 en {aantal_dozen} zitten")
+            doos = 1
+            continue
+        else:
+            return doos
 
 def controleer_inhoud(doos,dooskat):
     # kijk of opgegeven doos is doos waar kat zit
@@ -25,7 +39,6 @@ def controleer_inhoud(doos,dooskat):
         return True
     else:
         return False
-
 
 def verplaats_kat(doos_met_kat):
     # verplaats de kat, 1 naar links of rechts
@@ -50,8 +63,7 @@ def verplaats_kat(doos_met_kat):
         # -1 is naar links, 1 is naar rechts
         keuze=[-1,1]
         doos_met_kat += random.choice(keuze)
-
-
+    return
 
 # lus die loopt zolang kat gevonden = False
 # doos_met_kat = verstop_kat()
