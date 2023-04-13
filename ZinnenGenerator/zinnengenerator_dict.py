f40ba37ebf7d1126922c3396d41c3b956bbf27cc
W <wvandijk@mzh.nl>
2023-04-13 15:22:36 +0200

    dictionary's gevuld

---
+++ ZinnenGenerator/zinnengenerator_dict.py
@@ -0,0 +1,48 @@
+# Lijsten van woorden voor elk thema
+zelfstandige_naamwoorden = {
+    "voorwerpen": ["telefoon", "sleutel", "sjaal", "zonnebril"],
+    "meubels": ["bank", "bureau", "kruk", "kast"],
+    "mensen": ["vriend", "familie", "collega", "leraar"],
+    "dieren": ["hond", "kat", "konijn", "vogel"],
+    "voertuigen": ["fiets", "auto", "bus", "trein"],
+    "plaatsen": ["park", "strand", "supermarkt", "ziekenhuis"],
+    "gebouwen": ["kasteel", "museum", "restaurant", "hotel"],
+    "bomen": ["eik", "beuk", "den", "esdoorn"],
+    "planten": ["cactus", "orchidee", "tulp", "varen"]
+}
+
+lijdende_voorwerpen = {
+    "voorwerpen": ["kabel", "oplader", "adapter", "toetsenbord"],
+    "meubels": ["kussen", "plaid", "stoelkussen", "onderzetter"],
+    "mensen": ["brief", "cadeau", "paraplu", "tas"],
+    "dieren": ["bal", "speeltje", "riem", "voerbak"],
+    "voertuigen": ["helm", "stoelhoes", "sneeuwkettingen", "dakdragers"],
+    "plaatsen": ["karretje", "tas", "parasol", "koelbox"],
+    "gebouwen": ["menukaart", "servet", "wijnkaart", "asbak"],
+    "bomen": ["blad", "eikel", "dop", "tak"],
+    "planten": ["bloem", "blad", "pot", "gieter"]
+}
+
+bijvoeglijke_naamwoorden = {
+    "voorwerpen": ["roze", "glanzend", "oud", "duurzaam"],
+    "meubels": ["comfortabel", "modern", "klassiek", "verstelbaar"],
+    "mensen": ["sympathiek", "slim", "grappig", "aardig"],
+    "dieren": ["schattig", "speels", "lief", "trouw"],
+    "voertuigen": ["snel", "efficiënt", "ruim", "luxueus"],
+    "plaatsen": ["druk", "schoon", "gezellig", "veilig"],
+    "gebouwen": ["romantisch", "chique", "historisch", "uniek"],
+    "bomen": ["groot", "oud", "mooi", "geurend"],
+    "planten": ["groen", "bont", "gezond", "kleurrijk"]
+}
+
+werkwoorden = {
+    "voorwerpen": ["vasthouden", "opladen", "gebruiken", "verliezen"],
+    "meubels": ["schuiven", "verstellen", "monteren", "schoonmaken"],
+    "mensen": ["ontmoeten", "helpen", "bezoeken", "adviseren"],
+    "dieren": ["voeden", "aaien", "trainen", "uitlaten"],
+    "voertuigen": ["besturen", "onderhouden", "wassen", "parkeren"],
+    "plaatsen": ["bezoeken", "wandelen", "shoppen", "picknicken"],
+    "gebouwen": ["bezichtigen", "reserveren", "betreden", "verlaten"],
+    "bomen": ["planten", "snoeien", "verzorgen", "kappen"],
+    "planten": ["water geven", "verpotten", "snoeien", "mesten"]
+}
