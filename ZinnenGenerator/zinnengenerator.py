0e3d52bded2a2a617e34a2d081ca859367e1ddcb
Lucas Tepper <lucas@kryten.nl>
2023-04-13 15:31:34 +0200

    alternatief lidwoord

--- ZinnenGenerator/zinnengenerator.py
+++ ZinnenGenerator/zinnengenerator.py
@@ -10,9 +10,9 @@ def lidwoord():
 # alternatief
 #def lidwoord(zelfstandig_naamwoord):
 #    if zelfstandig_naamwoord[0] in ['a', 'e', 'i', 'o', 'u']:
-#        return 'het'
+#        return random.choice['het','een']
 #    else:
-#        return 'de'
+#        return random.choice['de', 'een']
 
 
 # huiswerk tijdelijke bestanden
@@ -39,5 +39,6 @@ def lidwoord():
 """
 - dictionary van: lidwoord, zelfstandig_naamwoord, lijdend_voorwerp, bijvoeglijk_naamwoord, werkwoord, koppelwoord
 - functie met als aanroep, dictionary, geeft terug random entry
+- bij zin, eerst zelfstandig naamwoord, dan lidwoord
 """
 print(lidwoord())
