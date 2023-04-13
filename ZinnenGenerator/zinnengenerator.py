8eaa92eeb4d570ca9f8fb336c44119e93fb21486
Lucas Tepper <lucas@kryten.nl>
2023-04-13 15:18:48 +0200

    eerste versie

---
+++ ZinnenGenerator/zinnengenerator.py
@@ -0,0 +1,35 @@
+# zinnengenerator
+import random
+# bestand met daarin de dictionaries
+#import zinnengenerator_dict.py
+
+def lidwoord():
+    lidwoorden = ["de", "het", "een"]
+    return random.choice(lidwoorden)
+
+# huiswerk tijdelijke bestanden
+# vermeld onder je module welke functies er in zitten
+#
+#import lucas.py
+#
+#
+#import bert.py
+#
+#
+#import frank.py
+#
+#
+#import wouter.py
+#
+#
+#import tom.py
+#
+#
+#import erwin.py
+#
+#
+"""
+- dictionary van: lidwoord, zelfstandig_naamwoord, lijdend_voorwerp, bijvoeglijk_naamwoord, werkwoord, koppelwoord
+- functie met als aanroep, dictionary, geeft terug random entry
+"""
+print(lidwoord())
