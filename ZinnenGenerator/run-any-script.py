55c3aa9fd5e7aebc1b6bc2534c333ea71de824e8
Lucas Tepper <lucas@kryten.nl>
2023-04-17 16:56:28 +0200

    .

--- ZinnenGenerator/run-any-script.py
+++ ZinnenGenerator/run-any-script.py
@@ -12,7 +12,7 @@ def process():
     # macos
     #output = subprocess.check_output(["/opt/homebrew/bin/python3.9", "/Users/lucas/stack/Python/itoperations/ZinnenGenerator/zinnengenerator.py"])
     # windows
-    output = subprocess.check_output(["python", "c:\temp\zinnengenerator.py"])
+    output = subprocess.check_output(["python", "c:\\Users\\tepperl\\Python\\it-operations-python-training\\ZinnenGenerator\\zinnengenerator.py"])
     return output.decode()
 
 app.run(host='0.0.0.0', port=8080)
