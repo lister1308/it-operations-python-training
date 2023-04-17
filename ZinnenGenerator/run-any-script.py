b293e7de54709d1aa36089457398f042407f0e00
Lucas Tepper <lucas@kryten.nl>
2023-04-17 22:53:03 +0200

    .

--- ZinnenGenerator/run-any-script.py
+++ ZinnenGenerator/run-any-script.py
@@ -1,6 +1,7 @@
 from flask import Flask, request, render_template
 import subprocess
 import platform
+from googlesearch import search
 
 app = Flask(__name__)
 
@@ -14,6 +15,20 @@ def process():
         output = subprocess.check_output(["python", "c:\\Users\\tepperl\\Python\\it-operations-python-training\\ZinnenGenerator\\zinnengenerator.py"])
     else:
         output = subprocess.check_output(["/opt/homebrew/bin/python3.9", "/Users/lucas/stack/Python/itoperations/ZinnenGenerator/zinnengenerator.py"])
-    return output.decode()
+    # zoek naar afbeeldingen
+    """
+    zin = output.decode().strip()
+    urls = []
+    for url in search(zin, num_results=10):
+        print(url)
+        if url.endswith(('.jpg', '.jpeg', '.png')):
+            urls.append(url)
+            break
+
+    # print de URL
+    #print(urls[0])
+    print(urls)
+    """
+    return output.decode().strip()
 
 app.run(host='0.0.0.0', port=8080)
