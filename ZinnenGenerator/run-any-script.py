9a19dec2b8892183df04ebaa1b9ebffb86f697c1
Lucas Tepper <lucas@kryten.nl>
2023-04-17 21:36:14 +0200

    .

--- ZinnenGenerator/run-any-script.py
+++ ZinnenGenerator/run-any-script.py
@@ -1,5 +1,6 @@
 from flask import Flask, request, render_template
 import subprocess
+import platform
 
 app = Flask(__name__)
 
@@ -9,10 +10,10 @@ def index():
 
 @app.route("/process", methods=["POST"])
 def process():
-    # macos
-    #output = subprocess.check_output(["/opt/homebrew/bin/python3.9", "/Users/lucas/stack/Python/itoperations/ZinnenGenerator/zinnengenerator.py"])
-    # windows
-    output = subprocess.check_output(["python", "c:\\Users\\tepperl\\Python\\it-operations-python-training\\ZinnenGenerator\\zinnengenerator.py"])
+    if platform.system() == 'Windows':
+        output = subprocess.check_output(["python", "c:\\Users\\tepperl\\Python\\it-operations-python-training\\ZinnenGenerator\\zinnengenerator.py"])
+    else:
+        output = subprocess.check_output(["/opt/homebrew/bin/python3.9", "/Users/lucas/stack/Python/itoperations/ZinnenGenerator/zinnengenerator.py"])
     return output.decode()
 
 app.run(host='0.0.0.0', port=8080)
