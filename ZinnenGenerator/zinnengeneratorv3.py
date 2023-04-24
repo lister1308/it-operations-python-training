ea41beb28d4fb97c9b6a74cf85b87f3f30d5897c
W <wvandijk@mzh.nl>
2023-04-24 11:08:01 +0200

    Opschoning

--- ZinnenGenerator/zinnengeneratorv3.py
+++ ZinnenGenerator/zinnengeneratorv3.py
@@ -1,11 +1,9 @@
 # zinnengenerator
-import random
 import argparse
 import TekstNaarSpraak
-import zinnengenerator_dictv3 as woordenlijsten
 from zinnengenerator_taal import *
 
-# argumenten voor als code vanuit webinterface wordt aangeroepen
+# Argumenten voor als code vanuit webinterface wordt aangeroepen
 parser = argparse.ArgumentParser(description="Omschrijving: genereer een random zin of zelfs complete alinea")
 parser.add_argument('--samenhang', action='store_true', help='Is er samenhang tussen de zinnen, default 1 = ja, of 0 = nee')
 parser.add_argument('--print', default='alinea', help='Print een zin of alinea')
@@ -17,15 +15,6 @@ args = parser.parse_args()
 if args.thema == 'random':
     args.thema = None
 
-
-# huiswerk:
-# Functies opnemen in een module
-# keuze van de woorden in de dictionaries: voorstel Wouter verder uitwerken
-# fun factor diverse mogelijkheden: web (opties in webinterface voor thema/aantal/geluid/zin vs alinea) 
-# wordt thema goed gebruikt nu, themacontrole? Bert
-# plaatjes van henny, rudi, evert maken ahv zakelijk tekenen: Tom
-# yoda mode (osv mode), groningse mode
-#
 if args.print == 'zin':
     for _ in range(int(args.aantal)):
        mijn_zin = Volzin(thema=args.thema)
